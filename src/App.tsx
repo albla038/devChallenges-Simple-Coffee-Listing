66aa5ffda5d479a6e3f2f49a16a6f3845a3b9eb0
Albert Lännerström <albert.lannerstrom@gmail.com>
2024-08-09 15:45:21 +0200

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="">
+      Whereas disregard and contempt for human rights have resulted
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: { sans: ["DM Sans", "sans-serif"] },
+    },
+    colors: {
+      black: "#111315",
+      "gray-dark": "#1B1D1F",
+      "gray-medium": "#6F757C",
+      "green-light": "#BEE3CC",
+      white: "#FEF7EE",
+      yellow: "#F6C768",
+      orange: "#ED735D",
+    },
+  },
+  plugins: [],
+};
